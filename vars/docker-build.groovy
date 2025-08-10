a0cfbbbaf0a8bf99daf0568038229a0ce9f97819
Kartik <154986110+KARTIKNAIK18@users.noreply.github.com>
2025-08-10 17:42:21 +0530

    Update docker-build.groovy

--- vars/docker-build.groovy
+++ vars/docker-build.groovy
@@ -1,4 +1,6 @@
 // Define function
 def call(String ProjectName, String ImageTag, String DockerHubUser){
+    sh "pwd"
+    sh "ls -l"
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
